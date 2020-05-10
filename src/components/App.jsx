85b31da481a819cb2d14eb33908afe31bad1aaf7
Christine Ting <christ123092@yahoo.com>
2020-05-10 20:31:47 +0800

    made top page responsive

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,89 @@
-import React, { useState } from 'react';
-import Icons from './Icons';
-import Paint from './Paint';
-import About from './About';
-import Work from './Work';
-import Resume from './Resume';
-import Contact from './Contact';
+import React, { useState, useEffect } from "react";
+import Icons from "./Icons";
+import Paint from "./Paint";
+import About from "./About";
+import Work from "./Work";
+import Resume from "./Resume";
+import Contact from "./Contact";
 
 const App = () => {
-  const [ filter, changeFilter ] = useState('brightness(100%) grayscale(1)');
-  const [ color, setColor ] = useState(false);
+  const [filter, changeFilter] = useState("brightness(100%) grayscale(1)");
+  const [color, setColor] = useState(false);
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth
+  });
+  const [nameFontSize, setNameFontSize] = useState(80);
+  const [roleFontSize, setRoleFontSize] = useState(20);
+  const [tabsFontSize, setTabsFontSize] = useState(17);
+  const [tabsFlex, setTabsFlex] = useState('row');
+  const [renderIcons, setIconsView] = useState(true);
+
+  const checkToResize = () => {
+    setNameFontSize(80);
+    setRoleFontSize(20);
+    setTabsFontSize(17);
+    setIconsView(true);
+    setTabsFlex('row');
+    if (window.innerWidth < 1150) {
+      setNameFontSize(50);
+      setRoleFontSize(20);
+      setTabsFlex('column');
+    }
+    if (window.innerWidth < 1464) {
+      setTabsFontSize(13);
+    }
+    if (window.innerWidth < 800) {
+      setNameFontSize(25);
+      setRoleFontSize(10);
+      setIconsView(false);
+    } 
+  };
+
+  React.useEffect(() => {
+    checkToResize();
+    const handleResize = () => {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth
+      });
+      checkToResize();
+    };
+    window.addEventListener("resize", handleResize);
+
+    return _ => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
 
   const clickToChangeFilter = () => {
     setColor(!color);
     if (!color) {
-      changeFilter('brightness(100%)');
+      changeFilter("brightness(100%)");
     } else {
-      changeFilter('brightness(100%) grayscale(1)');
+      changeFilter("brightness(100%) grayscale(1)");
     }
   };
 
   return (
-    <div className="main-view">
-      <div className="top-image" style={{filter}}/>
-      <div className="title-top">
-        <div id="name">CHRISTINE TING</div>
-        <div id="role">SOFTWARE ENGINEER</div>
+    <div className='main-view'>
+      {console.log(window.innerWidth, window.innerHeight)}
+      <div className='top-image' style={{ filter }} />
+      <div className='title-top'>
+        <div id='name' style={{ fontSize: nameFontSize }}>
+          CHRISTINE TING
+        </div>
+        <div id='role' style={{ fontSize: roleFontSize }}>SOFTWARE ENGINEER</div>
       </div>
-      <div id="tabs">
-        <div id="tab">ABOUT</div>
-        <div id="tab">WORK</div>
-        <div id="tab">RESUME</div>
-        <div id="tab">CONTACT</div>
+      <div id='tabs' style={{ fontSize: tabsFontSize, flexDirection: tabsFlex }}>
+        <div id='tab'>ABOUT</div>
+        <div id='tab'>WORK</div>
+        <div id='tab'>RESUME</div>
+        <div id='tab'>CONTACT</div>
       </div>
-      <Icons />
-      <Paint clickToChangeFilter={clickToChangeFilter} color={color}/>
-      <div className="main-info">
+      { renderIcons && <Icons /> }
+      <Paint clickToChangeFilter={clickToChangeFilter} color={color} />
+      <div className='main-info'>
         <About />
       </div>
       <Work />
