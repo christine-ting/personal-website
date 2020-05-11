dfedf9a784626ff3f7aa2b409a5b15b653118067
Christine Ting <christ123092@yahoo.com>
2020-05-11 10:44:39 +0800

    made contact form responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => (
-  <div className="contact">
-    <div className="contact-title">
-      CONTACT
+  <div className='contact'>
+    <div className='contact-title'>CONTACT</div>
+    <div className='contact-message'>
+      Drop me an e-mail at christ123092@gmail.com or simply fill out the form
+      below and I will reply to you soon!
     </div>
-    <div>
-      Drop me an e-mail at christ123092@gmail.com or simply fill out the form below and I will reply to you soon!
-    </div>
-    <div className="contact-form">
-      <div id="input">
-        <input id="name-input" type="text" name="name" placeholder="Name"/>
+    <div className='contact-form'>
+      <div>
+        <input id='name-input' type='text' name='name' placeholder='Name' />
       </div>
-      <div id="input">
-        <input id="email-input" type="text" name="email" placeholder="E-mail address"/>
+      <div>
+        <input
+          id='email-input'
+          type='text'
+          name='email'
+          placeholder='E-mail address'
+        />
       </div>
-      <div id="input">
-        <input id="message-input" type="text" name="message" placeholder="Your message"/>
+      <div>
+        <textarea
+          id='message-input'
+          type='text'
+          name='message'
+          placeholder='Your message'
+          rows="6" 
+          cols="50"
+        />
       </div>
-      <button className="send-btn">SEND</button>
+
+      <button className='send-btn'>SEND</button>
     </div>
   </div>
 );
