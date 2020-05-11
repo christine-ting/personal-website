9686e48b91719bfb2dcc885d435108a70deda534
Christine Ting <christ123092@yahoo.com>
2020-05-11 14:30:24 +0800

    can now send email to myself with contact form

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
-// const router = require('./router');
-
 const app = express();
 const port = 3000;
+const sendEmail = require('./sendEmail');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'));
 
-// app.use('/api', router);
+app.post('/api/form', sendEmail)
 
 app.listen(port, () => console.log(`personal website listening at http://localhost:${port}`));
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,38 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
-const Contact = () => (
-  <div className='contact'>
-    <div className='contact-title'>CONTACT</div>
-    <div className='contact-message'>
-      Drop me an e-mail at christ123092@gmail.com or simply fill out the form
-      below and I will reply to you soon!
+const Contact = () => {
+  const [input, setInput] = useState({ name: '', email: '', message: '' });
+  const changeHandler = (e) => {
+    setInput(input => ({...input, [event.target.name]: event.target.value}))
+  }
+  const sendMessage = () => {
+    axios.post('/api/form', input)
+         .then(() => console.log('message sent!'))
+         .catch((err) => console.error(err));
+    setInput({ name: '', email: '', message: '' });
+  }
+  
+  return (
+    <div className='contact'>
+      <div className='contact-title'>CONTACT</div>
+      <div className='contact-message'>
+        Drop me an e-mail at christineting.an@gmail.com or simply fill out the form
+        below and I will reply to you soon!
+      </div>
+      <div className='contact-form'>
+        <div>
+          <input id='name-input' type='text' name='name' placeholder='Name' value={input.name} onChange={(e) => changeHandler(e)} />
+        </div>
+        <div>
+          <input
+            id='email-input'
+            type='text'
+            name='email'
+            placeholder='E-mail address'
+            value={input.email}
+            onChange={(e) => changeHandler(e)}
+          />
+        </div>
+        <div>
+          <textarea
+            id='message-input'
+            type='text'
+            name='message'
+            placeholder='Your message'
+            rows="6" 
+            cols="50"
+            value={input.message}
+            onChange={(e) => changeHandler(e)}
+          />
+        </div>
+        <button className='send-btn' onClick={() => sendMessage()}>SEND</button>
+      </div>
     </div>
-    <div className='contact-form'>
-      <div>
-        <input id='name-input' type='text' name='name' placeholder='Name' />
-      </div>
-      <div>
-        <input
-          id='email-input'
-          type='text'
-          name='email'
-          placeholder='E-mail address'
-        />
-      </div>
-      <div>
-        <textarea
-          id='message-input'
-          type='text'
-          name='message'
-          placeholder='Your message'
-          rows="6" 
-          cols="50"
-        />
-      </div>
-
-      <button className='send-btn'>SEND</button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Contact;
