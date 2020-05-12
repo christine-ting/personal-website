535e38e84b94d8666739b10046705d4a421a8f3c
Christine Ting <christ123092@yahoo.com>
2020-05-12 21:01:08 +0800

    cleaned up code

--- client/src/components/Tabs.jsx
+++ client/src/components/Tabs.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
-const Test = ({ scrollToSection }) => {
+const categories = [ 'ABOUT', 'WORK', 'RESUME', 'CONTACT' ];
+
+const Tabs = ({ scrollToSection }) => {
   return (
     <div id='tabs'>
-      <div name="about" onClick={(e) => scrollToSection(e)}>ABOUT</div>
-      <div name="work" onClick={(e) => scrollToSection(e)}>WORK</div>
-      <div name="resume" onClick={(e) => scrollToSection(e)}>RESUME</div>
-      <div name="contact" onClick={(e) => scrollToSection(e)}>CONTACT</div>
+      { categories.map((category, index) => (
+        <div name={category.toLowerCase()} onClick={(e) => scrollToSection(e)} key={index}>{category}</div>
+      )) }
     </div>
   );
 };
 
-export default Test;
+export default Tabs;
