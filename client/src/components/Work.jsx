9d867ff42101eaf419a10b0ae982b9cbb2c10072
Christine Ting <christ123092@yahoo.com>
2020-05-14 19:57:57 +0800

    cleaned up code

--- client/src/components/Work.jsx
+++ client/src/components/Work.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React from 'react';
 
 const Work = ({ applications }) => {
+  
   const linkToPage = url => {
-    window.open(url, "_blank");
+    window.open(url, '_blank');
   };
 
   return (
-    <div className='all-work'>
-      <div className='work-title'>WORK</div>
+    <div className="all-work">
+      <div className="work-title">WORK</div>
       {applications.map((application, index) => {
         const {
           name,
@@ -18,33 +19,35 @@ const Work = ({ applications }) => {
           url
         } = application;
         return (
-          <div className='work' key={index}>
-            {name === "Personal Trainer Finder" ? (
-              <img className='gym-app-img' src={image} />
-            ) : name === "Trafalgar System Design Project" ? (
-              <div className='app-img-view-sdc'>
-                <div className='app-img-text'>Three EC2 Instances with Nginx Caching and Load Balancing</div>
-                <img className='app-img-sdc' src={image} />
+          <div className="work" key={index}>
+            {name === 'Personal Trainer Finder' ? (
+              <img className="gym-app-img" src={image} />
+            ) : name === 'Trafalgar System Design Project' ? (
+              <div className="app-img-view-sdc">
+                <div className="app-img-text">
+                  Three EC2 Instances with Nginx Caching and Load Balancing
+                </div>
+                <img className="app-img-sdc" src={image} />
               </div>
             ) : (
-              <div className='app-img-view'>
-                <img className='app-img' src={image} />
+              <div className="app-img-view">
+                <img className="app-img" src={image} />
               </div>
             )}
-            <div className='app-description'>
-              <p style={{ fontWeight: "bold" }}>{name}</p>
+            <div className="app-description">
+              <p style={{ fontWeight: 'bold' }}>{name}</p>
               <p>
                 {description}
                 <br />
-                Built with: {technologies.join(" | ")}
+                Built with: {technologies.join(' | ')}
               </p>
-              <div className='work-btn'>
-                <button className='view-repo' onClick={() => linkToPage(repo)}>
+              <div className="work-btn">
+                <button className="view-repo" onClick={() => linkToPage(repo)}>
                   VIEW REPO
                 </button>
                 {url && (
                   <button
-                    className='visit-site'
+                    className="visit-site"
                     onClick={() => linkToPage(url)}
                   >
                     VISIT SITE
