ecd13a9cc9a09b0e7aa48845ac03f6ed6a809b17
Christine Ting <christ123092@yahoo.com>
2020-05-15 16:15:02 +0800

    cleaned up code

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,60 +1,73 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const Contact = ({ contact }) => {
+
   const [input, setInput] = useState({ name: '', email: '', message: '' });
-  const changeHandler = (e) => {
-    setInput(input => ({...input, [event.target.name]: event.target.value}))
-  }
+
+  const changeHandler = () => {
+    setInput(input => ({ ...input, [event.target.name]: event.target.value }));
+  };
+
   const sendMessage = () => {
     if (input.name !== '' && input.email !== '' && input.message !== '') {
-      axios.post('/api/form', input)
-           .then(() => alert('Thank you for your message, I will reply to you soon!'))
-           .catch((err) => console.error(err));
+      axios
+        .post("/api/form", input)
+        .then(() =>
+          alert("Thank you for your message. I will reply to you soon!")
+        )
+        .catch(err => console.error(err));
       setInput({ name: '', email: '', message: '' });
     } else {
       for (var key in input) {
         if (input[key] === '') {
-          alert(`Please fill out your ${key}!`)
+          alert(`Please fill out your ${key}!`);
           break;
         }
       }
     }
-  }
-  
+  };
+
   return (
-    <div className='contact'>
-      <div className='contact-title'>CONTACT</div>
-      <div className='contact-message'>
-        {contact}
-      </div>
-      <div className='contact-form'>
+    <div className="contact">
+      <div className="contact-title">CONTACT</div>
+      <div className="contact-message">{contact}</div>
+      <div className="contact-form">
         <div>
-          <input id='name-input' type='text' name='name' placeholder='Name' value={input.name} onChange={(e) => changeHandler(e)} />
+          <input
+            id="name-input"
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={input.name}
+            onChange={() => changeHandler()}
+          />
         </div>
         <div>
           <input
-            id='email-input'
-            type='text'
-            name='email'
-            placeholder='E-mail address'
+            id="email-input"
+            type="text"
+            name="email"
+            placeholder="E-mail address"
             value={input.email}
-            onChange={(e) => changeHandler(e)}
+            onChange={() => changeHandler()}
           />
         </div>
         <div>
           <textarea
-            id='message-input'
-            type='text'
-            name='message'
-            placeholder='Your message'
-            rows="6" 
+            id="message-input"
+            type="text"
+            name="message"
+            placeholder="Your message"
+            rows="6"
             cols="50"
             value={input.message}
-            onChange={(e) => changeHandler(e)}
+            onChange={() => changeHandler()}
           />
         </div>
-        <button className='send-btn' onClick={() => sendMessage()}>SEND</button>
+        <button className="send-btn" onClick={() => sendMessage()}>
+          SEND
+        </button>
       </div>
     </div>
   );
