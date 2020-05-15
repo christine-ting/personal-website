bcd156c9ef9d24c3730d066180203d195bd3b2fd
Christine Ting <christ123092@yahoo.com>
2020-05-15 12:31:25 +0800

    completed responsiveness for resume

--- client/src/components/Applications.jsx
+++ client/src/components/Applications.jsx
@@ -2,31 +2,23 @@ import React from 'react';
 
 const Applications = ({ applications }) => (
   <div className="applications">
-    { applications.map((application, index) => {
+    {applications.map((application, index) => {
       const { name, technologies, description, details } = application;
       return (
         <div className="my-app" key={index}>
           <div className="app-title">
-            <div style={{fontWeight: 400}}>
-              {name}
-            </div>
-            <div>
-              {technologies.join(' | ')}
-            </div>
+            <div style={{ fontWeight: 400 }}>{name}</div>
+            <div>{technologies.join(" | ")}</div>
           </div>
-          <div className="app-summary">
-            {description}
-          </div>
+          <div className="app-summary">{description}</div>
           <div className="app-details">
-            { details.map((point, index) => (
-              <p key={index}>
-                &#9675;&nbsp;{point}
-              </p>
-            )) }
+            {details.map((point, index) => (
+              <p key={index}>&#9675;&nbsp;{point}</p>
+            ))}
           </div>
         </div>
-      )
-    }) }
+      );
+    })}
   </div>
 );
 
--- client/src/components/Experience.jsx
+++ client/src/components/Experience.jsx
@@ -1,38 +1,33 @@
 import React from 'react';
+
 const Experience = ({ experience }) => (
   <div className="experience">
-    { experience.map((work, index) => {
+    {experience.map((work, index) => {
       const { name, place, titles, details } = work;
       return (
         <div className="job" key={index}>
-          <div className="job-title" style={{fontWeight: 400}}>
+          <div className="job-title" style={{ fontWeight: 400 }}>
             {name} | {place}
           </div>
           <div className="roles">
-            { titles.map((role, index) => {
+            {titles.map((role, index) => {
               const { title, time } = role;
               return (
                 <div className="role-duration" key={index}>
-                  <div>
-                    {title}
-                  </div>
-                  <div>
-                    {time}
-                  </div>
+                  <div>{title}</div>
+                  <div>{time}</div>
                 </div>
-              )
-            }) }
+              );
+            })}
           </div>
           <div className="job-details">
-            { details.map((point, index) => (
-              <p key={index}>
-                &#9675;&nbsp;{point}
-              </p>
-            )) }
+            {details.map((point, index) => (
+              <p key={index}>&#9675;&nbsp;{point}</p>
+            ))}
           </div>
         </div>
-      )
-    }) }
+      );
+    })}
   </div>
 );
 
