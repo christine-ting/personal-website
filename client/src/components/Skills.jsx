fd590ad09d2a74f20977f3dee88210d45725b1b9
Christine Ting <christ123092@yahoo.com>
2020-05-15 11:51:11 +0800

    cleaned up code

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 
-const Skills = ({ skills }) => {
-  return (
-    <div className="skills">
-      {skills.map((skill, index) => {
-        const { category, technologies } = skill;
-        return (
-          <div key={index}>
-            <span style={{ fontWeight: 400 }}>{category}:&nbsp;</span>
-            {technologies.join(" | ")}
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+const Skills = ({ skills }) => (
+  <div className="skills">
+    {skills.map((skill, index) => {
+      const { category, technologies } = skill;
+      return (
+        <div key={index}>
+          <span style={{ fontWeight: 400 }}>{category}:&nbsp;</span>
+          {technologies.join(" | ")}
+        </div>
+      );
+    })}
+  </div>
+);
 
 export default Skills;
